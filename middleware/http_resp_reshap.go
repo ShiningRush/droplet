6eb79562b8c7036618313e08261322a1e903216e
ShiningRush <277040271@qq.com>
2023-03-31 22:33:38 +0800

    chore: refactor codes

--- middleware/http_resp_reshap.go
+++ middleware/http_resp_reshap.go
@@ -16,39 +16,31 @@ func NewRespReshapeMiddleware(respNewFunc func() data.HttpResponse) *HttpRespRes
 }
 
 func (mw *HttpRespReshapeMiddleware) Handle(ctx core.Context) error {
-	code, message := 0, ""
-	var d interface{}
-	if err := mw.BaseMiddleware.Handle(ctx); err != nil {
-		switch t := err.(type) {
-		case *data.BaseError:
-			code, message, d = t.Code, t.Message, t.Data
-		default:
-			code, message = data.ErrCodeInternal, err.Error()
-		}
-		var resp data.HttpResponse
-		if r, ok := ctx.Output().(data.HttpResponse); ok {
-			resp = r
-		} else {
-			resp = mw.respNewFunc()
-		}
-		resp.Set(code, message, d)
-		resp.SetReqID(ctx.GetString(KeyRequestID))
-		ctx.SetOutput(resp)
-		// response reshape is the last step, so we don't need to return error
-		return nil
-	}
+	handlerErr := mw.BaseMiddleware.Handle(ctx)
 
-	switch ctx.Output().(type) {
+	var resp data.HttpResponse
+	switch t := ctx.Output().(type) {
 	case data.RawHttpResponse, data.HttpFileResponse:
+		return nil
 	case data.HttpResponse:
-		resp := ctx.Output().(data.HttpResponse)
-		resp.SetReqID(ctx.GetString(KeyRequestID))
+		resp = t
 	default:
-		resp := mw.respNewFunc()
-		resp.Set(code, message, ctx.Output())
-		resp.SetReqID(ctx.GetString(KeyRequestID))
+		// wrap result
+		resp = mw.respNewFunc()
+		resp.Set(0, "", ctx.Output())
 		ctx.SetOutput(resp)
 	}
 
+	resp.SetReqID(ctx.GetString(KeyRequestID))
+	if handlerErr != nil {
+		switch t := handlerErr.(type) {
+		case *data.BaseError:
+			resp.Set(t.Code, t.Message, t.Data)
+		default:
+			resp.Set(data.ErrCodeInternal, handlerErr.Error(), nil)
+		}
+	}
+
+	// response reshape is the last step, so we don't need to return error
 	return nil
 }
