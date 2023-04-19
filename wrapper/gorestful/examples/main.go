ef014b2e3f16ff9f60c475c2464a95463f84b651
ShiningRush <277040271@qq.com>
2023-04-19 15:07:13 +0800

    chore: update wrapper version

---
+++ wrapper/gorestful/examples/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/emicklei/go-restful/v3"
+	"github.com/shiningrush/droplet/core"
+	"github.com/shiningrush/droplet/wrapper"
+
+	rwrap "github.com/shiningrush/droplet/wrapper/gorestful"
+)
+
+func main() {
+	ws := new(restful.WebService)
+	ws.Route(ws.POST("/json_input/{id}").To(rwrap.Wraps(JsonInputDo, wrapper.InputType(reflect.TypeOf(&JsonInput{})))))
+	restful.Add(ws)
+	http.ListenAndServe(":8080", nil)
+}
+
+type JsonInput struct {
+	ID    string   `auto_read:"id,path" json:"id"`
+	User  string   `auto_read:"user,header" json:"user"`
+	IPs   []string `json:"ips"`
+	Count int      `json:"count"`
+	Body  []byte   `auto_read:"@body"`
+}
+
+func JsonInputDo(ctx core.Context) (interface{}, error) {
+	input := ctx.Input().(*JsonInput)
+
+	return input, nil
+}
--- wrapper/gorestful/go.mod
+++ wrapper/gorestful/go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/emicklei/go-restful/v3 v3.9.0
 	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/shiningrush/droplet v0.2.4
+	github.com/shiningrush/droplet v0.4.2
 )
 
 replace github.com/shiningrush/droplet => ../../
